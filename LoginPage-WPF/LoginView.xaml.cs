db8007d8cc5e3b30363737fd4a7425593459a812
Breno Maistro <b.maistro@vicsamgroup.it>
2024-08-07 09:58:10 +0200

    Interface's first sketch

--- LoginPage-WPF/LoginView.xaml.cs
+++ LoginPage-WPF/LoginView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,15 +16,13 @@ using System.Windows.Shapes;
 namespace LoginPage_WPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logica di interazione per LoginView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginView : UserControl
     {
-        public MainWindow()
+        public LoginView()
         {
             InitializeComponent();
-
-            MainContent.Content = new LoginView();
         }
     }
 }
