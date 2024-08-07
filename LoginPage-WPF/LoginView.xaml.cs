b47dd7270dced64b5500ef4ff685562596c881ea
Breno Maistro <b.maistro@vicsamgroup.it>
2024-08-07 10:13:27 +0200

    Small design adjustments

--- LoginPage-WPF/LoginView.xaml.cs
+++ LoginPage-WPF/LoginView.xaml.cs
@@ -24,5 +24,10 @@ namespace LoginPage_WPF
         {
             InitializeComponent();
         }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
